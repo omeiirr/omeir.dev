09c180ca4bf51623780ce559a9fdff8701872d3b
Omeir <omeirf.02@gmail.com>
2022-06-28 02:59:22 +0530

    add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css';
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 
+import ReactGA from 'react-ga4';
+import '../styles/globals.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Add Google Analytics
+    ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID || '');
+    ReactGA.event('websiteOpened', {
+      event_category: 'APP',
+    });
+  }, []);
+
   return (
     <ThemeProvider attribute='class'>
       <Component {...pageProps} />
