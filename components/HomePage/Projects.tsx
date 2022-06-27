b56e274d781c4d9f727f5618819b56f8fa14771c
Omeir <omeirf.02@gmail.com>
2022-06-28 03:01:03 +0530

    new component: Projects

---
+++ components/HomePage/Projects.tsx
@@ -0,0 +1,12 @@
+const heading =
+  'mb-4 mt-4  text-2xl font-bold text-black md:text-3xl dark:text-white';
+const Projects = () => {
+  return (
+    <>
+      <h3 className={heading}>Projects</h3>
+      {/* <h3 className={subheading}>Languages</h3> */}
+    </>
+  );
+};
+
+export default Projects;
