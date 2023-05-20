389404aa9d4471f7134f70fa16d9021193d1062c
Omeir <omeirf.02@gmail.com>
2023-05-20 16:33:53 +0530

    add redirect links to resume and research paper

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['avatars.githubusercontent.com', 'user-images.githubusercontent.com'],
+    domains: [
+      'avatars.githubusercontent.com',
+      'user-images.githubusercontent.com',
+    ],
   },
   webpack(config) {
     config.module.rules.push({
@@ -11,5 +14,21 @@ module.exports = {
     });
 
     return config;
+  },
+  async redirects() {
+    return [
+      {
+        source: '/resume',
+        destination:
+          'https://drive.google.com/file/d/1Af6E_eCtapnAysOZAWeuUgzEUAneFgMo/view?usp=sharing',
+        permanent: true,
+      },
+      {
+        source: '/publications/ecsdlt',
+        destination:
+          'https://drive.google.com/file/d/1I9HWrsDKy8cMwc6jxncCVUDowOHoWrCc/view',
+        permanent: true,
+      },
+    ];
   },
 };
