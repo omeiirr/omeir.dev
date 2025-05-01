cabaffb4cb0edb500424c63509f1dee7f934f4af
Omeir Fawaz <54888682+omeiirr@users.noreply.github.com>
2025-05-01 20:44:12 +0530

    add logos

--- components/HomePage/Skills.tsx
+++ components/HomePage/Skills.tsx
@@ -11,6 +11,13 @@ import MySQL from 'assets/tech-icons/MySQL.svg';
 import JavaScript from 'assets/tech-icons/JavaScript.svg';
 import TypeScript from 'assets/tech-icons/TypeScript.svg';
 
+import Azure from 'assets/tech-icons/Azure.svg';
+import PowerAutomate from 'assets/tech-icons/PowerAutomate.svg';
+import PowerBi from 'assets/tech-icons/PowerBi.svg';
+import Purview from 'assets/tech-icons/Purview.svg';
+import Python from 'assets/tech-icons/Python.svg';
+import SharePoint from 'assets/tech-icons/SharePoint.svg';
+
 const commonSvgConfig = {
   width: '52px',
   height: '52px',
@@ -20,6 +27,17 @@ const Skills = () => {
   return (
     <>
       <h3 className='heading-text'>Skills</h3>
+      <h3 className='subheading-text'>Data</h3>
+      <div className='flex flex-wrap items-center justify-start gap-8 mb-6 '>
+        <PowerBi {...commonSvgConfig} />
+        <SharePoint {...commonSvgConfig} />
+        <Purview {...commonSvgConfig} />
+        <PowerAutomate {...commonSvgConfig} />
+        <Python {...commonSvgConfig} />
+        <Azure {...commonSvgConfig} />
+      </div>
+
+
       <h3 className='subheading-text'>Languages</h3>
       <div className='flex flex-wrap items-center justify-start gap-8 mb-6 '>
         <JavaScript {...commonSvgConfig} />
