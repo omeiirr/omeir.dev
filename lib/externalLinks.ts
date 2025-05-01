bf55bfe39c8cccd2a77cdfa0af8479521fee3efd
Omeir Fawaz <54888682+omeiirr@users.noreply.github.com>
2025-05-02 01:09:14 +0530

    update resume

--- lib/externalLinks.ts
+++ lib/externalLinks.ts
@@ -1,4 +1,4 @@
 export const resumeLink =
-  'https://drive.google.com/file/d/10as690RDbcyvwbVdXThYwtcf66CNcmLZ/view?usp=sharing';
+  'https://drive.google.com/file/d/1vopVEJty8h6GVkOvOi1WvXs8c1SDGaWQ/view?usp=sharing';
 
 export const blogLink = 'https://blog.omeir.dev';
